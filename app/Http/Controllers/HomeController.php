23d2ffa60a6671f61e92a7f81caac7eaf799c16d
khanh_nguyen <zhuukhanhz@gmail.com>
2017-07-20 14:28:37 +0700

    private chat

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+use App\User;
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = Auth::user();
+        $users = User::all();
+        return view('hometest',compact('user','users'));
+    }
+}
