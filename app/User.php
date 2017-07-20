c5869a5482598bd8951176d491f9cae2b7ea3ab7
linhnm <linhnm@bap.jp>
2017-07-20 15:54:37 +0700

    change_avatar_default

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'level', 'fullname',
+        'name', 'email', 'password', 'level', 'fullname','avatar'
     ];
 
     /**
