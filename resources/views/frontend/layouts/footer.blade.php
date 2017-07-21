e6a939d62a5cf6784be382d525a82abe0e29799c
khanh_nguyen <zhuukhanhz@gmail.com>
2017-07-21 11:58:34 +0700

    console chat

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -2,10 +2,7 @@
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js"></script>
-<script>
-	var user = {!!json_encode($user)!!};
-	console.log(user);
-</script>
+@widget('GetUserLogin')
 @yield('script');
 </body>
 
