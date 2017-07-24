94c06ebf257d3f7a4805268df249cd682b537030
npthang <npthang02959@gmail.com>
2017-07-24 17:38:16 +0700

    sedder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(RoomsTableSeeder::class);
+        $this->call(PrivatesTableSeeder::class);
+        $this->call(MessengesTableSeeder::class);
+        $this->call(FilesTableSeeder::class);
+        /*$this->call(EmotionsTableSeeder::class);*/
     }
 }
