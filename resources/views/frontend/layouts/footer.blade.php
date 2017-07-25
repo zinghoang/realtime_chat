ace112866fe75b158d9bfdb7ea4e64cbfeaa5f79
loitv <loitv@bap.jp>
2017-07-25 16:50:39 +0700

    edit the name of room by ajax

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
+
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js"></script>
