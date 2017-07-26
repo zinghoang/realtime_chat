82f7a00ba84fe32f9673e1afa9f482ee705247a6
loitv <loitv@bap.jp>
2017-07-26 14:21:58 +0700

    Edit Acount

--- app/Http/Requests/AccountRequest.php
+++ app/Http/Requests/AccountRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UserRequest extends FormRequest
+class AccountRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -26,9 +26,8 @@ class UserRequest extends FormRequest
         return [
             'name' => 'required|min:2|max:32',
             'email' => 'required|email|min:10|max:200',
-            'password' => 'required|min:6|max:32|confirmed',
             'fullname' => 'required|min:2|max:100',
-            'level' => 'required|numeric|integer|between:0,1'
+            'password' => 'max:32|confirmed',
         ];
     }
 }
